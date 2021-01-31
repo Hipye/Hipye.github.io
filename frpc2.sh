a6be6bdbc71b9304c5a3fca4bd3365857c01cab0
hipye <i@omoe.cc>
2021-01-31 09:29:54 +0800

    initdfrp

---
+++ frpc2.sh
@@ -0,0 +1,18 @@
+#!/bin/sh /etc/rc.common
+# Copyright (C) 2008 OpenWrt.org
+
+START=99
+USE_PROCD=1
+PROG="/bin/frpc_linux_arm" #..................frp...............
+
+start_service() {
+    rm -f /frpc_overload*.log* # remove old logt/logp files
+    procd_open_instance
+    procd_set_param command "$PROG"
+ #  procd_append_param command -f 002f8228a9b3c455:799243
+    procd_append_param command -f 002f8228a9b3c455:829272
+    procd_set_param stdout 1 # forward stdout of the command to logd
+    procd_set_param stderr 1 # same for stder
+    procd_set_param respawn
+    procd_close_instance
+}
