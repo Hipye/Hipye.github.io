19f38371422506fca2357b7a9efa3da4f58890ce
hipye <i@omoe.cc>
2021-02-03 05:16:47 +0800

    fixhugonewpost

--- hugonewpost.sh
+++ hugonewpost.sh
@@ -17,37 +17,37 @@ basedirectory=/mnt/sdcard/Documents/PureWriter
 hugodirectory=~/xxxx
 hugogeneral=~/xxxx/content/posts
 
-echo "\033[34m\033[01m	=======================\033[0m"
-echo "\033[33m\033[01m	键入博客文件名\033[0m"
-read -p "	" newpost && sleep 2
-echo "\033[34m\033[01m	生成文件\033[0m" && sleep 1
+echo "\033[32m\033[01m	=======================\033[0m"
+echo "\033[32m\033[01m	\n	键入博客文件名\n\033[0m"
+read -p "	: " newpost && sleep 2
+rm ${newpost}.md && rm ${hugogeneral}/${newpost}.md && rm ${hugogeneral}/${newpost}-.md
+echo "\033[32m033[01m	删除旧有文件\033[0m"
 cd ${hugodirectory} && pwd && sleep 1
-rm ${newpost}.md && echo "\033[32m033[01m	基础设定\033[0m"
+echo "\033[32m\033[01m	\n	生成文件\033[0m" && sleep 1
 hugo new ${hugogeneral}/${newpost}.md
 sleep 1
 hugo new ${hugogeneral}/${newpost}-.md
 sleep 5 && cd
-echo "\033[34m\033[01m	写入文件\033[0m"
+echo "\033[32m\033[01m	写入文件\033[0m"
 sleep 1
 cd ${basedirectory}
 sleep 1
 pwd
 mv ${newpost}.md ~/
-sleep 1
-cd
+sleep 1 && cd
 rm -rf ${hugogeneral}/${newpost}-.md
 cat ${hugogeneral}/${newpost}.md ${newpost}.md>${hugogeneral}/${newpost}-.md
 sleep 1
 rm -rf ${hugogeneral}/${newpost}.md
-sleep 1 && echo "\033[34m\033[01m	修改.md为可部署状态\033[0m"
+sleep 1 && echo "\033[32m\033[01m	修改.md为可部署状态\033[0m"
 sed  -i '4s/draft: true/draft: false/g' ${hugogeneral}/${newpost}-.md 
 cd xxxx && hugo && sleep 3
 cd public && pwd
-echo "\033[34m\033[01m	推送\033[0m" && sleep 1
+echo "\033[32m\033[01m	推送\033[0m" && sleep 1
 git add .
 git commit -m "update"
 git push && sleep 2
-echo "\033[34m\033[01m	ok bye\033[0m" && sleep 2
-echo "\033[34m\033[01m	=======================\033[0m"
+echo "\033[32m\033[01m	ok bye\033[0m" && sleep 2
+echo "\033[32m\033[01m	=======================\033[0m"
 
 
