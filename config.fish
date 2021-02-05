314f8577e235b711455be29b6f12a6d05bfa8998
hipye <i@omoe.cc>
2021-02-06 06:51:18 +0800

    update

--- config.fish
+++ config.fish
@@ -1,8 +1,19 @@
+alias sai "sudo apt install -y"
+alias sug "sudo apt update && sudo apt upgrade -y"
+alias sar "sudo apt remove -y"
+alias sas "sudo apt search"
+alias ar "apt remove -y"
+alias as "apt search"
 alias ai "apt install -y"
-alias aa "aria2c -x 16"
 alias ug "apt update && apt upgrade -y"
-alias gitconfig "git config --global user.email "i@omoe.cc" && git config --global user.name "hipye""
+
 alias cdd "cd /data/data/com.termux/files"
 alias zzz "ssh moriz@192.168.8.171"
 alias xxx "ssh root@192.168.8.234"
 alias ccc "ssh root@192.168.8.1"
+
+alias gpp "git add . && git commit -m "update" && git push"
+alias gitconfig "git config --global user.email "i@omoe.cc" && git config --global user.name "hipye" && git config --global pull.rebase true && git config --global branch.autoSetupRebase always"
+alias gc "git clone"
+
+alias aa "aria2c -x 16"
