ed04ed9785a5c5182a96479761b28a0ace0a1251
hipye <i@omoe.cc>
2021-02-05 08:00:39 +0800

    link

--- config.fish
+++ config.fish
@@ -1,4 +1,1 @@
-alias sai "sudo apt install -y"
-alias aa "aria2c -x 16"
-alias sug "sudo apt update && sudo apt upgrade -y"
-alias gitconfig "git config --global user.email "i@omoe.cc" && git config --global user.name "hipye"" 
+.config/fish/config.fish
