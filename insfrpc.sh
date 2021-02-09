deedfe587be95eaa2714f06012a8ffe365141b3e
hipye <i@omoe.cc>
2021-02-09 16:55:05 +0800

    update

--- insfrpc.sh
+++ insfrpc.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 wget https://getfrp.sh/d/frpc_linux_arm
 chmod +x frpc_linux_arm && mv frpc_linux_arm /bin/
-wget https://gitee.com/hipye/st/raw/master/frpc.sh
-wget https://gitee.com/hipye/st/raw/master/frpc1.sh
-wget https://gitee.com/hipye/st/raw/master/frpc2.sh
-chmod +x frpc.sh && mv frpc.sh /etc/init.d/ && /etc/init.c/frpc.sh enable
-chmod +x frpc1.sh && mv frpc1.sh /etc/init.d/ && /etc/init.c/frpc1.sh enable
-chmod +x frpc2.sh && mv frpc2.sh /etc/init.d/ && /etc/init.c/frpc2.sh enable
+wget https://gitee.com/hipye/st/raw/master/frpc
+wget https://gitee.com/hipye/st/raw/master/frpc1
+wget https://gitee.com/hipye/st/raw/master/frpc2
+chmod +x frpc && mv frpc /etc/init.d/ && /etc/init.c/frpc enable
+chmod +x frpc1 && mv frpc1 /etc/init.d/ && /etc/init.c/frpc1 enable
+chmod +x frpc2 && mv frpc2 /etc/init.d/ && /etc/init.c/frpc2 enable
 
 
 
