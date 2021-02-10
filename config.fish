ce104e0b36e205f754d4f5c696fba2e514a3666a
hipye <i@omoe.cc>
2021-02-10 12:35:53 +0800

    update

--- config.fish
+++ config.fish
@@ -17,4 +17,4 @@ alias gitconfig "git config --global user.email "i@omoe.cc" && git config --glob
 alias gc "git clone"
 
 alias aa "aria2c -x 16"
-alias cpfish "cp st/config.fish .config/fish/ && fish"
+alias cpfish "cp st/config.fish ~/.config/fish/ && fish"
