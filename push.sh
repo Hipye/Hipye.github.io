f3c5c7a69c5de2085a1d39f78af443d92d91ab21
hipye <i@omoe.cc>
2021-02-14 10:59:30 +0800

    “init”

--- push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-read -p "       请输入链接: " commit
-git add .
-git commit -m "${commit}"
-git push
--- sync.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd && sleep 2
-read -p "       请输入链接: " rclone
-rclone sync ${rclone} moriz:backups
